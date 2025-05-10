f061fde514d2b789d18eebbadf6f375c38b5e933
Nishant Gupta <niishant666@gmail.com>
2025-05-10 14:32:24 +0530

    Project

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function display(a){
+let data = document.getElementById("disp")
+if(data.innerHTML=='0'){
+data.innerHTML = a
+}else{
+    data.innerHTML=data.innerHTML+a
+}
+
+}
+
+
+function u_clear(){
+    let data=document.getElementById("disp")
+    data.innerHTML=0
+}
+
+function calculate(){
+    let data= document.getElementById("disp")
+    let expression= data.innerHTML
+    console.log(expression)
+    let res= eval(expression)
+    data.innerHTML=res
+}
